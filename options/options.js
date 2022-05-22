e89e4b0cef4513be1f35beef7a4206c351b7c6db
Crowdbotics <team@crowdbotics.com>
2022-05-22 20:23:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kingdom_wealth_inst_35380",
+  url: "https://kingdom_wealth_inst_35380.botics.co",
+  api: "https://kingdom_wealth_inst_35380.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
